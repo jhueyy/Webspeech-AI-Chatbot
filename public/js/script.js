c60260b41ab11661b8a01bba06655703f45f5a95
Jake Huey <jakehuey@gmail.com>
2024-10-29 11:06:11 -0700

    voice recognition working, but error on response token

--- public/js/script.js
+++ public/js/script.js
@@ -1,44 +1,55 @@
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-recognition.lang = 'en-US';
-recognition.interimResults = false;
-
-document.querySelector('button').addEventListener('click', () => {
-    recognition.start();
-  });
-
-recognition.addEventListener('result', (e) => {
-    let last = e.results.length - 1;
-    let text = e.results[last][0].transcript;
-
-    console.log('Confidence: ' + e.results[0][0].confidence);
-// sockets are the best solution for bidirectional communication, 
-//especially when pushing an event from the server to the browser
+'use strict';
 
 const socket = io();
-socket.emit('chat message', text);
 
-//takes a string and enables browser to speak w/ text
-//create reference to api entry point
-//create speech synthesis using its constructor to text to be synthesized when word or sound is spoken
-//https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance
-//we can set the voice 
+const outputYou = document.querySelector('.output-you');
+const outputBot = document.querySelector('.output-bot');
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+document.querySelector('button').addEventListener('click', () => {
+  recognition.start();
+});
+
+recognition.addEventListener('speechstart', () => {
+  console.log('Speech has been detected.');
+});
+
+recognition.addEventListener('result', (e) => {
+  console.log('Result has been detected.');
+
+  let last = e.results.length - 1;
+  let text = e.results[last][0].transcript;
+
+  outputYou.textContent = text;
+  console.log('Confidence: ' + e.results[0][0].confidence);
+
+  socket.emit('chat message', text);
+});
+
+recognition.addEventListener('speechend', () => {
+  recognition.stop();
+});
+
+recognition.addEventListener('error', (e) => {
+  outputBot.textContent = 'Error: ' + e.error;
+});
 
 function synthVoice(text) {
-    const synth = window.speechSynthesis;
-    const utterance = new SpeechSynthesisUtterance();
-    utterance.text = text;
-    synth.speak(utterance);
-  }
-  
+  const synth = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance();
+  utterance.text = text;
+  synth.speak(utterance);
+}
+
 socket.on('bot reply', function(replyText) {
-    synthVoice(replyText);
-}); 
-  
-  
+  synthVoice(replyText);
 
-
-
-
+  if(replyText == '') replyText = '(No answer...)';
+  outputBot.textContent = replyText;
 });
-  
